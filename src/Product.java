d36b6f1cc10e2cf62a0c43b8dade034728ae0b80
Rey <reynaldoortega13@gmail.com>
2022-11-11 11:33:53 -0600

    added print method in Product Class  and test Toys and functing as expected in Main

--- src/Product.java
+++ src/Product.java
@@ -2,13 +2,17 @@ public class Product {
 
     String name;
 
-    int price;
+    double price;
 
     boolean inStock;
     //commit
-    public Product(String name, int price, boolean inStock) {
+    public Product(String name, double price, boolean inStock) {
         this.name = name;
         this.price = price;
         this.inStock = inStock;
+    }
+
+    public void print(){
+        System.out.println("---- " + this.name + " ----" + this.price + " ---- " + this.inStock);
     }
 }
--- src/Toys.java
+++ src/Toys.java
@@ -1,2 +1,9 @@
-public class Toys {
+import javafx.scene.Parent;
+
+public class Toys extends Product {
+
+
+    public Toys(String name, double price, boolean inStock) {
+        super(name, price, inStock);
+    }
 }
