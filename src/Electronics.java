591f927423da94a64b2b545d5edcc9a16742c40a
camabdulkadir <camaarabdul32@gmail.com>
2022-11-11 12:10:36 -0600

    Electronics

--- src/Electronics.java
+++ src/Electronics.java
@@ -1,2 +1,11 @@
 public class Electronics {
+    String name;
+    boolean inStock;
+    double price;
+
+    public Electronics(String name, boolean inStock, double price) {
+        this.name = name;
+        this.inStock = inStock;
+        this.price = price;
+    }
 }
