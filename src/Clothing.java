b568ede1a3ab755c23a411c09975126b106bf060
markeviuscollier <Markevius.collier@gmail.com>
2022-11-11 12:26:12 -0600

    "Clothing update"

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,19 +1,10 @@
-public class Clothing  {
+public class Clothing extends Product  {
 
     String name;
     double price;
     boolean inStock;
 
-    public Clothing(String name, double price, boolean inStock) {
-        this.name = name;
-        this.price = price;
-        this.inStock = inStock;
+    public Clothing(String name, double price, boolean inStock){
+        super(name, price, inStock);
     }
-
-    Clothing shirt = new Clothing("Shirt", 10.99, true);
-    Clothing pants = new Clothing("Pants", 24.99, false);
-    Clothing hat = new Clothing("hat", 12.99, true);
-    Clothing af1 = new Clothing("Air Force 1", 84.99, true);
-
-
 }
