5867ace8f7e8ae049b0333820590b996a528499a
markeviuscollier <Markevius.collier@gmail.com>
2022-11-11 11:34:59 -0600

    "Clothing update"

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,2 +1,19 @@
-public class Clothing {
+public class Clothing  {
+
+    String name;
+    double price;
+    boolean inStock;
+
+    public Clothing(String name, double price, boolean inStock) {
+        this.name = name;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    Clothing shirt = new Clothing("Shirt", 10.99, true);
+    Clothing pants = new Clothing("Pants", 24.99, false);
+    Clothing hat = new Clothing("hat", 12.99, true);
+    Clothing af1 = new Clothing("Air Force 1", 84.99, true);
+
+
 }
